f1d9a6a0f451f2e0d960fdaf2057b89eba7ae995
ALI <68592145+Ali-awesome@users.noreply.github.com>
2022-05-24 13:20:07 +0600

    fontawesome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is home </h1>
+      <h1>This is home <FontAwesomeIcon icon={faSearch} />  </h1>
+
     </div>
   );
 }

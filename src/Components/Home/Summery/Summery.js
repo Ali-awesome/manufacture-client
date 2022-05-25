dcfbf05dbd03ec6113ff12c97e308fb6cded10b8
ALI <68592145+Ali-awesome@users.noreply.github.com>
2022-05-25 13:53:37 +0600

    Business Summery Done

--- src/Components/Home/Summery/Summery.js
+++ src/Components/Home/Summery/Summery.js
@@ -1,60 +1,54 @@
+import { faCoins, faList, faSearch, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import fact1 from '../../../images/icon-image/fact1.png';
-import fact2 from '../../../images/icon-image/fact2.png';
-import fact3 from '../../../images/icon-image/fact3.png';
-import fact4 from '../../../images/icon-image/fact4.png';
+import CountUp from 'react-countup';
 import './Summery.css';
 
 const Summery = () => {
     return (
-        <div id="facts" class="facts-area bg-neutral">
-            <div class="container">
-                <div class="facts-wrapper">
-
-                    <div class="ts-facts">
-                        <div class="ts-facts-img">
-                            <img loading="lazy" src={fact1} alt="facts-img" />
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp" data-count="1789">0</span></h2>
-                            <h3 class="ts-facts-title">Total Projects</h3>
-                        </div>
+        <div id="facts" className='container flex flex-row gap-2 m-5'>
+            <div class="card w-96 bg-base-100 shadow-xl image-full">
+                <figure><img src="https://i.postimg.cc/d0gGNG4Q/construction.jpg" alt="Tools" /></figure>
+                <div class="card-body flex justify-center align-middle text-center">
+                    <FontAwesomeIcon className='fa fa-4x icon' icon={faSearch} />
+                    <div className='text-7xl'>
+                        <CountUp end={1000} duration={5}></CountUp><span>+</span>
                     </div>
-
-                    <div class="ts-facts">
-                        <div class="ts-facts-img">
-                            <img loading="lazy" src={fact2} alt="facts-img" />
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp" data-count="647">0</span></h2>
-                            <h3 class="ts-facts-title">Staff Members</h3>
-                        </div>
-                    </div>
-
-                    <div class="ts-facts">
-                        <div class="ts-facts-img">
-                            <img loading="lazy" src={fact3} alt="facts-img" />
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp" data-count="4000">0</span></h2>
-                            <h3 class="ts-facts-title">Hours of Work</h3>
-                        </div>
-                    </div>
-
-                    <div class="ts-facts">
-                        <div class="ts-facts-img">
-                            <img loading="lazy" src={fact4} alt="facts-img" />
-                        </div>
-                        <div class="ts-facts-content">
-                            <h2 class="ts-facts-num"><span class="counterUp" data-count="44">0</span></h2>
-                            <h3 class="ts-facts-title">Countries Experience</h3>
-                        </div>
-                    </div>
-
-
+                    <h2 className='text-xl'>Tools to Choose From</h2>
                 </div>
             </div>
-        </div>
+            <div class="card w-96 bg-base-100 shadow-xl image-full">
+                <figure><img src="https://i.postimg.cc/d0gGNG4Q/construction.jpg" alt="Tools" /></figure>
+                <div class="card-body flex justify-center align-middle text-center">
+                    <FontAwesomeIcon className='fa fa-4x icon' icon={faCoins} />
+                    <div className='text-7xl'>
+                        <CountUp end={50} duration={5}></CountUp><span>M+</span>
+                    </div>
+                    <h2 className='text-xl'>Revenue</h2>
+                </div>
+            </div>
+            <div class="card w-96 bg-base-100 shadow-xl image-full">
+                <figure><img src="https://i.postimg.cc/d0gGNG4Q/construction.jpg" alt="Tools" /></figure>
+                <div class="card-body flex justify-center align-middle text-center">
+                    <FontAwesomeIcon className='fa fa-4x icon' icon={faUsers} />
+                    <div className='text-7xl'>
+                        <CountUp end={33} duration={5}></CountUp><span>K+</span>
+                    </div>
+                    <h2 className='text-xl'>Satisfied Customer</h2>
+                </div>
+            </div>
+            <div class="card w-96 bg-base-100 shadow-xl image-full">
+                <figure><img src="https://i.postimg.cc/d0gGNG4Q/construction.jpg" alt="Tools" /></figure>
+                <div class="card-body flex justify-center align-middle text-center">
+                    <FontAwesomeIcon className='fa fa-4x icon' icon={faList} />
+                    <div className='text-7xl'>
+                        <CountUp end={100} duration={5}></CountUp><span>+</span>
+                    </div>
+                    <h2 className='text-xl'>Reviews</h2>
+                </div>
+            </div>
+
+        </div >
     );
 };
 
